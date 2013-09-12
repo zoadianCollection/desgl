bc468e8eb3e757b219935cd3129fca7780a7f794
deviator <code.viator@gmail.com>
2013-09-13 00:37:26 +0400

    rshape added

---
+++ import/desgl/draw/rshape.d
@@ -0,0 +1,76 @@
+/++
+прямоугольник, удобен для наложения текстур
++/
+module desgl.draw.rshape;
+
+import derelict.opengl3.gl3;
+
+import desmath.types.vector,
+       desmath.types.rect;
+
+import desgl.object;
+import desgl.texture;
+
+alias vrect!int irect;
+alias GLTexture!2 GLTexture2D;
+
+import desutil.logger;
+debug mixin( LoggerPrivateMixin( "rshape", __MODULE__ ) );
+
+class RectShape: GLObj!()
+{
+protected:
+    static float[] colArray( in col4 c )
+    { return c.data ~ c.data ~ c.data ~ c.data; }
+
+    static float[] colArray( in col4[4] c )
+    { return c[0].data ~ c[1].data ~ c[2].data ~ c[3].data; }
+
+    GLTexture2D tex;
+    int use_tex = 0;
+
+public:
+
+    this( ShaderProgram sp )
+    {
+        debug log.trace( "rhape ctor start" );
+        super( sp );
+        debug log.trace( "rhape base class ctor" );
+        auto pos = new buffer( "pos", GL_ARRAY_BUFFER, 
+                [ 0.0f, 0, 1, 0, 0, 1, 1, 1 ], GL_STATIC_DRAW );
+        pos.setAttribPointer( "vertex", 2, GL_FLOAT );
+        debug log.trace( "pos vbo" );
+        auto uv = this.new buffer( "uv", GL_ARRAY_BUFFER, 
+                [ 0.0f, 0, 1, 0, 0, 1, 1, 1 ], GL_STATIC_DRAW );
+        uv.setAttribPointer( "uv", 2, GL_FLOAT );
+        debug log.trace( "uv vbo" );
+        auto col = this.new buffer( "col", GL_ARRAY_BUFFER, 
+                colArray( col4( 1,1,1,1 ) ) ); 
+        col.setAttribPointer( "color", 4, GL_FLOAT );
+        debug log.trace( "col vbo: ", colArray( col4(1,1,1,1) ) );
+        tex = new GLTexture2D( ivec2(1,1) ); 
+        debug log.trace( "tex ctor" );
+        draw.connect( (){ glDrawArrays( GL_TRIANGLE_STRIP, 0, 4 ); } );
+        draw.addPair( (){ 
+                    shader.setUniform!int( "use_texture", use_tex );
+                    shader.setUniform!int( "ttu", GL_TEXTURE0 );
+                    if( use_tex ) tex.bind(); 
+                },
+                (){ if( use_tex ) tex.unbind(); } );
+        debug log.info( "rhape ctor finish" );
+    }
+
+    void setColor( in col4 c ){ vbo["col"].data( colArray( c ) ); }
+
+    void notUseTexture(){ use_tex = 0; }
+
+    void fillAlphaTexture(T,E)( in T sz, E[] data )
+        if( isCompVector!(2,int,T) && ( is( E == ubyte ) ) )
+    { 
+        static if( is( E == ubyte ) ) enum type = GL_UNSIGNED_BYTE;
+        tex.image( sz, GL_RED, GL_RED, type, data ); 
+        use_tex = 1;
+    }
+
+    void reshape( in irect r ) { vbo["pos"].data( r.points!float ); }
+}
