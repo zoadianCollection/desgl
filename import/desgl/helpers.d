a7cd5a6d7aeaf7e83a8bb0b542fdbed50e1ffb1c
deviator <code.viator@gmail.com>
2013-10-05 05:40:15 +0400

    some help functions

---
+++ import/desgl/helpers.d
@@ -0,0 +1,39 @@
+module desgl.helpers;
+
+import derelict.opengl3.gl3;
+
+import desutil.logger;
+debug mixin( LoggerPrivateMixin( "glhelper", __MODULE__ ) );
+
+nothrow @property void checkGL( string md=__FILE__, int ln=__LINE__ )
+{
+    auto err = glGetError();
+    try 
+    {
+    if( err != GL_NO_ERROR )
+    {
+        import std.stdio : stderr;
+        stderr.writefln( " ## GL ERROR ## %s at line: %s: 0x%04x", md, ln, err );
+    }
+    else
+    {
+        import std.string : format;
+        debug log.trace( format( "GL OK %s at line: %s", md, ln ) );
+    }
+    } catch( Exception e ){}
+}
+
+pure nothrow string toDString( const(char*) c_str )
+{
+    string buf;
+    char *ch = cast(char*)c_str;
+    while( *ch != '\0' ) buf ~= *(ch++);
+    return buf;
+}
+
+pure nothrow string toDStringFix(size_t S)( const(char[S]) c_buf )
+{
+    string buf;
+    foreach( c; c_buf ) buf ~= c;
+    return buf;
+}
