6e6396faf3778986d41328671dc15c171395d48c
deviator <code.viator@gmail.com>
2013-10-08 16:44:33 +0400

    add viewport control

--- import/desgl/helpers.d
+++ import/desgl/helpers.d
@@ -10,17 +10,18 @@ nothrow @property void checkGL( string md=__FILE__, int ln=__LINE__ )
     auto err = glGetError();
     try 
     {
-    if( err != GL_NO_ERROR )
-    {
-        import std.stdio : stderr;
-        stderr.writefln( " ## GL ERROR ## %s at line: %s: 0x%04x", md, ln, err );
-    }
-    else
-    {
-        import std.string : format;
-        debug log.trace( format( "GL OK %s at line: %s", md, ln ) );
-    }
-    } catch( Exception e ){}
+        if( err != GL_NO_ERROR )
+        {
+            import std.stdio : stderr;
+            stderr.writefln( " ## GL ERROR ## %s at line: %s: 0x%04x", md, ln, err );
+        }
+        else
+        {
+            import std.string : format;
+            debug log.trace( format( "GL OK %s at line: %s", md, ln ) );
+        }
+    } 
+    catch( Exception e ){}
 }
 
 pure nothrow string toDString( const(char*) c_str )
@@ -54,3 +55,71 @@ pure nothrow T[] dataArray(size_t N, T, string AS)( in vec!(N,T,AS)[] arr... )
     return ret;
 }
 
+import desmath.types.rect;
+import std.array;
+
+class ViewportStateCtrl
+{
+private:
+    struct PRect { irect viewport, scissor; }
+
+    PRect[] states;
+    PRect current;
+
+    void update()
+    {
+        auto vp = current.viewport;
+        auto sc = current.scissor;
+        glViewport( vp.x,vp.y,vp.w,vp.h );
+        glScissor( sc.x,sc.y,sc.w,sc.h );
+    }
+
+    nothrow static void setFromGL( ref PRect rr )
+    {
+        glGetIntegerv( GL_VIEWPORT, rr.viewport.vec_rect.data.ptr );
+        glGetIntegerv( GL_SCISSOR_BOX, rr.scissor.vec_rect.data.ptr );
+    }
+
+public:
+
+    this( in irect init ) { set( init ); }
+    this(){ setFromGL( current ); }
+
+    void push() { states ~= current; }
+
+    void pull()
+    {
+        if( !states.empty )
+        {
+            current = states.back();
+            states.popBack();
+        }
+        update();
+    }
+
+    uint sub( in irect vp )
+    {
+        auto cvp = current.viewport;
+        auto csc = current.scissor;
+
+        irect nvp;
+        nvp.x = cvp.x + vp.x;
+        nvp.y = cvp.y + cvp.h - ( vp.y + vp.h );
+        nvp.w = vp.w;
+        nvp.h = vp.h;
+
+        current.viewport = nvp;
+        current.scissor = csc.overlap( nvp );
+
+        update();
+
+        return current.scissor.area;
+    }
+
+    void set( in irect vp )
+    {
+        current.viewport = vp;
+        current.scissor = vp;
+        update();
+    }
+}
